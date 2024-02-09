b600a53b497de9af5b9dc8ed8cb56616fb1e852d
Hannah Bradley <hannahthesailor@yahoo.com>
2024-02-09 01:38:07 -0500

    phase-1-first-class-functions-lab.md

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+module.exports = {
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers,
+    selectingDrivers,
+};
+
+function createFareMultiplier(integer) {
+    return function(fare) {
+      return fare * integer;
+    }
+  };
+  
+  function fareDoubler(fare) {
+    const doubleFare = createFareMultiplier(2);
+    return doubleFare(fare);
+  }
+
+  function fareTripler(fare) {
+    const tripleFare = createFareMultiplier(3);
+    return tripleFare(fare);
+  }
+
+  function selectDifferentDrivers(drivers, returnDrivers) {
+    if (returnDrivers === returnFirstTwoDrivers) {
+      return drivers.slice(0, 2);
+    } else if (returnDrivers === returnLastTwoDrivers) {
+      return drivers.slice(-2);
+    }
+  }
